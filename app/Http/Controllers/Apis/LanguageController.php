a8ac072ad2a8a04af019be8f68409cdd7f75782a
Tan Nguyen <tannp27@gmail.com>
2022-09-11 19:03:09 +0700

    update permission all path

--- app/Http/Controllers/Apis/LanguageController.php
+++ app/Http/Controllers/Apis/LanguageController.php
@@ -11,6 +11,38 @@ class LanguageController extends Controller
     /**
      * @param $language
      * @return \Illuminate\Http\JsonResponse
+     *
+     * @QA\Info(
+     *     title="Change language",
+     *     description="Change language",
+     *     version="1.0.0"
+     * )
+     * @OA\PathItem(
+     *     path="/api/lang/change/{language}",
+     * )
+     * @QA\Get(
+     *     path="/api/lang/change/{language}",
+     *     description="Change language",
+     *     summary="Change language",
+     *     operationId="changeLanguage",
+     *     produces={"application/json"},
+     *     tags={"Language"},
+     *     @QA\Response(
+     *         response=200,
+     *         description="Change language successfully",
+     *         @QA\Schema(
+     *             type="object",
+     *             @QA\Property(property="signal", type="number"),
+     *             @QA\Property(property="status", type="string"),
+     *             @QA\Property(property="code", type="number"),
+     *             @QA\Property(property="data", type="array",
+     *                  @QA\Items(type="object",
+     *                       @QA\Property(property="language", type="string", example="en")
+     *                  )
+     *             ),
+     *         )
+     *     )
+     * )
      */
     public function changeLanguage($language)
     {
