2f1d089ae8bd6656b2c03fb782604c057d288bff
Tan Nguyen <tannp27@gmail.com>
2022-10-17 15:30:06 +0700

    MUS-4 create info modify common fields for migrate

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CommonFieldsProvider::class,
+        App\Providers\InfoCommonFieldsProvider::class,
 
     ],
 
